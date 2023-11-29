ca9494ad8e738c14355d37735680d5587a1d1fa4
Edwin Zuidema <edwin.zuidema@tomtom.com>
2023-11-29 16:31:26 +0100

    Copilot tab to Ctrl-J

--- init.lua
+++ init.lua
@@ -252,6 +252,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Remap Copilot 'tab' completion to Ctrl-J
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
